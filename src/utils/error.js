7ee1e439a1bcfc46654543be7f90ccc76f76b504
paul59720 <paul.mathon@hei.yncrea.fr>
2020-02-06 12:58:25 +0100

    Update Readme

--- src/utils/error.js
+++ src/utils/error.js
@@ -1,9 +1,8 @@
-class LearchError extends Error {
-
-    constructor(error, message) {
-        super(message);
-        this.error = error;
-        this.message = `[${error}] - ${message}`;
-        this.originalMessage = message;
-    }
+class SearchError extends Error {
+  constructor(error, message) {
+    super(message);
+    this.error = error;
+    this.message = `[${error}] - ${message}`;
+    this.originalMessage = message;
+  }
 }
